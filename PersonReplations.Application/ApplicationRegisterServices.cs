0c3f262b72b04a6158e9a70682e2a7f08f0f6db3
George <george.prozorov@gmail.com>
2023-11-08 20:55:22 +0400

    initial

---
+++ PersonReplations.Application/ApplicationRegisterServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonReplations.Application
+{
+  public static class ApplicationRegisterServices
+  {
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+      return services;
+    }
+  }
+}
---
+++ PersonReplations.Persistence/PersistenceRegisterServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonReplations.Persistence
+{
+  public static class PersistenceRegisterServices
+  {
+    public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services)
+    {
+      return services;
+    }
+  }
+}
