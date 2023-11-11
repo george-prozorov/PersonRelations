6bd0c48c0121c774731df252fb63a77d12f200b9
George <george.prozorov@gmail.com>
2023-11-11 16:07:26 +0400

    Add Migration

---
+++ PersonReplations.Persistence/Migrations/20231111120651_Initial.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PersonReplations.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(2737)),
+                    DisplayName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(5184)),
+                    DisplayName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(6341)),
+                    DisplayName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 599, DateTimeKind.Local).AddTicks(3038)),
+                    DisplayName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelationTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    GenderId = table.Column<int>(type: "int", nullable: false),
+                    PersonalId = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(7529))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Persons_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Relations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RelationTypeId = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 599, DateTimeKind.Local).AddTicks(1921))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Relations_RelationTypes_RelationTypeId",
+                        column: x => x.RelationTypeId,
+                        principalTable: "RelationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactTypeId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(4032))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_ContactTypes_ContactTypeId",
+                        column: x => x.ContactTypeId,
+                        principalTable: "ContactTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonRelations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    RelationId = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedAd = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 11, 11, 16, 6, 51, 598, DateTimeKind.Local).AddTicks(9487))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonRelations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonRelations_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonRelations_Relations_RelationId",
+                        column: x => x.RelationId,
+                        principalTable: "Relations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "DisplayName" },
+                values: new object[,]
+                {
+                    { 1, "თბილისი" },
+                    { 2, "პრაღა" },
+                    { 3, "პარიზი" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContactTypes",
+                columns: new[] { "Id", "DisplayName" },
+                values: new object[,]
+                {
+                    { 1, "მობილური" },
+                    { 2, "ოფისის" },
+                    { 3, "სახლის" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "DisplayName" },
+                values: new object[,]
+                {
+                    { 1, "ქალი" },
+                    { 2, "კაცი" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RelationTypes",
+                columns: new[] { "Id", "DisplayName" },
+                values: new object[,]
+                {
+                    { 1, "კოლეგა" },
+                    { 2, "ნაცნობი" },
+                    { 3, "ნათესავი" },
+                    { 4, "სხვა" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ContactTypeId",
+                table: "Contacts",
+                column: "ContactTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_PersonId",
+                table: "Contacts",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonRelations_PersonId",
+                table: "PersonRelations",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonRelations_RelationId",
+                table: "PersonRelations",
+                column: "RelationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_CityId",
+                table: "Persons",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_FirstName",
+                table: "Persons",
+                column: "FirstName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_GenderId",
+                table: "Persons",
+                column: "GenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_LastName",
+                table: "Persons",
+                column: "LastName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_PersonalId",
+                table: "Persons",
+                column: "PersonalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relations_RelationTypeId",
+                table: "Relations",
+                column: "RelationTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "PersonRelations");
+
+            migrationBuilder.DropTable(
+                name: "ContactTypes");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Relations");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+
+            migrationBuilder.DropTable(
+                name: "RelationTypes");
+        }
+    }
+}
